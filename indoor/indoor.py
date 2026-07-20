995a741371968eafdf83b3f3a79c7656d3edd89c
Ashraf Naeem <iamashrafnaeem@gmail.com>
2026-07-20 10:33:20 +0530

    Add solution, indoor voices

---
+++ indoor/indoor.py
@@ -0,0 +1,9 @@
+def main():
+    print("It's a program to change your input to lowercase letters completely.",
+          "Please enter your sentence below:", sep="\n")
+    sentence = input()
+    print(sentence.lower())
+
+
+if __name__ == "__main__":
+    main()
